7102806269c97489dfaf2e56d6b44d3692074966
Withered Flowers <withered-flowers@users.noreply.github.com>
2024-01-19 12:58:56 +0700

    feat: - implement htmx - implement simple form to fetch data using htmx (SimpleForm.astro)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+import htmx from "astro-htmx";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [tailwind(), htmx()],
+	output: "server",
+	adapter: node({
+		mode: "standalone",
+	}),
+});
